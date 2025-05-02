1cd4c039b8dd4c4299ee8af1fc7321e2c23e2283
Bradley Chen <bradley.y.chen@gmail.com>
2025-05-01 22:26:07 -0400

    first commit
    Shitty ass commit that does basically nothing

---
+++ index.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, jsonify, render_template, redirect,url_for, session
+from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+	return render_template('index.html')
