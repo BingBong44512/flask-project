d2099d5d5b302a1d44a104f4e9ffad3b14ea2cb0
Bradley Chen <bradley.y.chen@gmail.com>
2025-05-12 22:15:35 -0400

    dsgaf

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask_login import login_user, logout_user, current_user, UserMixin
+from flask_login import login_user, logout_user, current_user, UserMixin, login_required
 from app import app, login_manager
 from app.forms import LoginForm
 
 login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = "Please log in to access this page."
 
 class TempUser(UserMixin):
 	def __init__(self, id, username, password):
 		self.id = id
 		self.username = username
-		self.password = password
+		self.pass1 = password
 
 	def check_password(self, password):
-		return bool(password)
+		if(self.pass1==password):
+			return True
+		else:
+			return False
 
 temp_user = TempUser(1, "test", "admin")
 
@@ -36,13 +41,14 @@ def login():
 		password = form.password.data
 		remember = form.remember_me.data
 
+		print("akhfh")
 		if username == temp_user.username and temp_user.check_password(password):
 			login_user(temp_user, remember=remember)
 			flash('Logged in successfully.')
 			next_page = request.args.get('next')
 			if not next_page or not next_page.startswith('/'):
 				next_page = url_for('index')
-			return redirect(next_page)
+			return redirect(url_for('user', username=username))
 		else:
 			flash('Invalid username or password (temporary)')
 			return render_template('login.html', form=form)
@@ -51,3 +57,8 @@ def login():
 			return flask.abort(400)
 		return redirect(next or flask.url_for('user.html'))
 	return render_template('login.html', form=form)
+
+@app.route('/user/<username>')
+#@login_required
+def user(username):
+	return render_template('user.html', username=username)
