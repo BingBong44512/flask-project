3e1ad09374caf4d158c14391e4b6d1339c14b6ba
potato33s <burneee30@gmail.com>
2025-05-18 22:02:06 -0400

    progress, added selenium--thankfully it was easy, not I'm gonna start processing the info

--- scraper.py
+++ scraper.py
@@ -1,20 +1,31 @@
 from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
+import time
 import re
 
-def createSoup(url):
-	response = requests.get(url)
-	soup = BeautifulSoup(response.text, "html.parser")
+def getHTML(url):
+	wd = webdriver.Firefox()
+	wd.get(url)
+	html = wd.page_source
+	soup = BeautifulSoup(html, "html.parser")
+	wd.close()
 	return soup
 
 def glossary(url):
-	soup=createSoup(url)
-	txt = soup.find("tr")
-	print(soup)
+	soup = getHTML(url)
+	rawText = soup.find_all("p",{"class":"glossaryElement"})
+	cleanText = []
+	for text in rawText:
+		text = text.split(" | ")
+		clean = {text[0],text[1]}
+		cleanText.append()
+	return cleanText
 
 
-def text(url):
-	soup=createSoup(url)
+
+def main():
+	print(glossary("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)/zz%3A_Back_Matter/20%3A_Glossary"))
+	return 0
 
 if __name__=="__main__":
-	glossary("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)/zz%3A_Back_Matter/20%3A_Glossary")
+	main()
