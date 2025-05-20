8f0d2acd4739c763327148aefbe7a4b1603af13e
potato33s <burneee30@gmail.com>
2025-05-20 14:44:31 -0400

    Breadly, I did it :)

--- scraper.py
+++ scraper.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 import re
+import json
 
 def getHTML(url):
 	wd = webdriver.Firefox()
@@ -14,17 +15,21 @@ def getHTML(url):
 def glossary(url):
 	soup = getHTML(url)
 	rawText = soup.find_all("p",{"class":"glossaryElement"})
-	cleanText = []
-	for text in rawText:
-		text = text.split(" | ")
-		clean = {text[0],text[1]}
-		cleanText.append()
+	cleanText = {}
+	for texts in rawText:
+		texts = texts.text.split(" | ")
+		cleanText[texts[0]]=texts[1]
+	# print(re.search('<p class="glossaryElement"><span class="glossaryTerm">(.+)</span>',rawText[0]))
+		# text = text.split(" | ")
+		# cleanText.append({text[0],text[1]})
 	return cleanText
 
 
 
 def main():
-	print(glossary("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)/zz%3A_Back_Matter/20%3A_Glossary"))
+	diction = (glossary("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)/zz%3A_Back_Matter/20%3A_Glossary"))
+	with open("dict.json", "w") as saving:
+		json.dump(diction, saving, indent = 4)
 	return 0
 
 if __name__=="__main__":
