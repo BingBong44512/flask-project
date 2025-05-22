fe21ac4063bba73785c0eb138d63b97975f43bdd
potato33s <burneee30@gmail.com>
2025-05-22 12:44:52 -0400

    I think I am cooking

--- scraper.py
+++ scraper.py
@@ -4,32 +4,54 @@ import time
 import re
 import json
 
+wd = webdriver.Firefox()
+
 def getHTML(url):
-	wd = webdriver.Firefox()
 	wd.get(url)
 	html = wd.page_source
 	soup = BeautifulSoup(html, "html.parser")
-	wd.close()
 	return soup
 
-def glossary(url):
+def glossary(url,subject):
 	soup = getHTML(url)
 	rawText = soup.find_all("p",{"class":"glossaryElement"})
 	cleanText = {}
 	for texts in rawText:
 		texts = texts.text.split(" | ")
 		cleanText[texts[0]]=texts[1]
-	# print(re.search('<p class="glossaryElement"><span class="glossaryTerm">(.+)</span>',rawText[0]))
-		# text = text.split(" | ")
-		# cleanText.append({text[0],text[1]})
 	return cleanText
+	
 
+def getLinks(url):
+	soup = getHTML(url)
+	raw = soup.find_all("a", {"class":"mt-sortable-listing-link mt-edit-section internal"})
+	links = []
+	glossaryLink = ""
+	for link in raw:
+		link = link["href"]
+		if  ("Front Matter" not in link) and  ("Back Matter" not in link) and  ("Appendices" not in link):
+			links.append(link)
+	return links
+	
+
+
+
+
+def getData(url, subject):
+	diction = glossary(url+"/zz%3A_Back_Matter/20%3A_Glossary",subject)
+	with open((subject+"dict.json"), "w") as saving:
+		json.dump(diction, saving, indent = 4)
+
+	# getLinks(url)
 
 
 def main():
-	diction = (glossary("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)/zz%3A_Back_Matter/20%3A_Glossary"))
-	with open("chemdict.json", "w") as saving:
-		json.dump(diction, saving, indent = 4)
+	# subject = input("Subject?: ")
+	# url = input("Url?: ")
+
+	print(getLinks("https://chem.libretexts.org/Bookshelves/General_Chemistry/Chemistry_2e_(OpenStax)"))
+
+	wd.close()
 	return 0
 
 if __name__=="__main__":
