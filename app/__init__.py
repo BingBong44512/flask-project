b488510e4b8f625b98083a7643ac47ef3e042821
Bubby Bilas <bobertfilas@gmail.com>
2025-06-04 14:49:06 -0400

    We spent a whole period on this

--- app/__init__.py
+++ app/__init__.py
@@ -2,14 +2,14 @@ from flask import Flask
 from config import Config
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from common import cache
+from .common import cache
 
 app = Flask(__name__)
 app.config.from_object(Config)
 login_manager = LoginManager()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placeholder.db'
-cache.init_app(app=app, config={"CACHE_TYPE": "filesystem",'CACHE_DIR': Path('/tmp')})
+cache.init_app(app=app, config={"CACHE_TYPE": "SimpleCache"})
 cache.set("subject list", {"astro":[0,0],"bio":[0,0],"chem":[0,0],"physics":[0,0]})
 db = SQLAlchemy(app)
 
