071d8460f4f867fcf548ac3990e5a61d39d6d295
potato33s <burneee30@gmail.com>
2025-06-04 14:34:37 -0400

    doing some cache stuff

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,7 @@ login_manager = LoginManager()
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placeholder.db'
 cache.init_app(app=app, config={"CACHE_TYPE": "SimpleCache"})
 cache.set("subject list", {"astro":[0,0],"bio":[0,0],"chem":[0,0],"physics":[0,0]})
+cache.set("todaySubject","astro")
 db = SQLAlchemy(app)
 
 from app import routes
