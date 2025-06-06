55ae72386e0cf44db99b57655a795247f1eecbe7
potato33s <burneee30@gmail.com>
2025-06-06 12:30:04 -0400

    made the scheduler work

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from config import Config
 from app.common import cache
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+from random import choice
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,9 +11,25 @@ login_manager = LoginManager()
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placeholder.db'
 cache.init_app(app=app, config={"CACHE_TYPE": "SimpleCache"})
-cache.set("subject list", {"astro":[0,0],"bio":[0,0],"chem":[0,0],"physics":[0,0]})
+cache.set("subjectList", {"astro":[0,-1],"bio":[0,-1],"chem":[0,-1],"physics":[0,-1]})
 cache.set("todaySubject","astro")
 db = SQLAlchemy(app)
 
 from app import routes
-#https://stackoverflow.com/questions/21214270/how-to-schedule-a-function-to-run-every-hour-on-flask
+
+import atexit
+
+from apscheduler.schedulers.background import BackgroundScheduler
+
+def update():
+	subjects = cache.get("subjectList")
+	cache.set("todaySubject",choice([*subjects.keys()]))
+	subjects[cache.get("todaySubject")][1]+=1
+	cache.set("subjectList",subjects)
+
+
+def init_scheduler():
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=update, trigger="interval", seconds=5)
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
